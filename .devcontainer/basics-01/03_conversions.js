14314b4d81e734fa3b468d2d4716d3e90ee933ad
asadniaz12804 <asadniaz983@gmail.com>
2025-06-04 08:32:32 +0000

    v4

---
+++ .devcontainer/basics-01/03_conversions.js
@@ -0,0 +1,61 @@
+let number=46
+//console.log(typeof number) //Here it is of type number 
+let numTostring=String(number)
+//console.log(typeof numTostring) //Here its type is string
+let score="456"
+let stringTonum=Number(score)
+//console.log(stringTonum)
+//console.log(typeof stringTonum)
+
+// "33" => 33
+// "33abc" => NaN
+//  true => 1; false => 0
+
+let isloggedin="Asad"      
+//console.log(isloggedin)    //Asad
+//console.log(typeof isloggedin);   //String
+
+let booleanisloggedin=Boolean(isloggedin)   //Converted to Boolean
+//console.log(booleanisloggedin);             //True
+//console.log(typeof booleanisloggedin);      //Boolean
+
+// 1 => true; 0 => false
+// "" => false *
+// "Asad" => true
+
+       /*********************************************************************************** */
+let value = 3
+let negValue = -value
+//console.log(negValue);
+
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**3);
+// console.log(2/3);
+// console.log(2%3);
+
+
+let str1 = "hello"
+let str2 = " Asad"
+
+let str3 = str1 + str2
+//console.log(str3); //Two strings can be concatinate its ouput is "hello Asad"
+
+// console.log("1" + 2);
+// console.log(1 + "2");//Its also concatinate two diffrent datatypes but combines with + output=>12 for both
+
+// console.log("1" + 2 + 2);//122 ***(String + Number = String)=>"1"+2=>"12"=>"12"+2=>122
+// console.log(1 + 2 + "2");//32(The result of both are of string type)
+
+// console.log(+true);
+// console.log(+""); the + operator in front of a value (called the unary plus) tries to convert that value into a number.
+
+let num1, num2, num3
+
+num1 = num2 = num3 = 2 + 2
+
+let gameCounter = 100
+++gameCounter;
+//console.log(gameCounter);
